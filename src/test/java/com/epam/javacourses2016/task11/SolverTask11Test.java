ccb52456ae39e3fb24e47e88920ca97b0b5e3382
Ivan Fefilov <spb.fefilov.i@gmail.com>
2016-12-01 12:54:12 +0300

    Merge branch 'master' into Ivan-Fefilov

--- src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
+++ src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
@@ -1,5 +1,6 @@
 package com.epam.javacourses2016.task11;
 
+import com.epam.javacourses2016.task8.SolverTask8;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -7,6 +8,9 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
+
+import static org.testng.Assert.*;
 
 public class SolverTask11Test {
     @Test(enabled = true, dataProvider = "people")
--- src/test/java/com/epam/javacourses2016/task18/SolverTask18Test.java
+++ src/test/java/com/epam/javacourses2016/task18/SolverTask18Test.java
@@ -4,8 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.Stack;
-
 /**
  * Created by kodoo on 13.11.16.
  */
@@ -29,6 +27,34 @@ public class SolverTask18Test {
 
     private SolverTask18.RectangularIntegerMatrix convertToMatrix(int[][] arr) {
         return new Matrix(arr);
+    }
+
+    private class Matrix implements SolverTask18.RectangularIntegerMatrix {
+
+        private int[][] matrix;
+
+        public Matrix(int[][] data) {
+            this.matrix = data;
+        }
+
+        public int[][] getMatrix() {
+            return matrix;
+        }
+
+        @Override
+        public int getWidth() {
+            return matrix[0].length;
+        }
+
+        @Override
+        public int getHeight() {
+            return matrix.length;
+        }
+
+        @Override
+        public int getValue(int indexWidth, int indexHeight) {
+            return matrix[indexWidth][indexHeight];
+        }
     }
 
 }
